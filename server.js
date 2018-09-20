fcea7aff373c26b6036afe6c6f7b8b96f366f522
Jessica Lovell <jessicaamberlovell@gmail.com>
2018-09-20 11:15:02 -0700

    location

--- server.js
+++ server.js
@@ -1,47 +1,48 @@
 `use strict`;
 
-//Application dependencies
 const express = require('express');
-
 const superagent = require('superagent');
 const cors = require('cors');
+const pg = require('pg');
 
-//this allows us to join front-end and back-end files from different folders
-
-//loads environment variables from .env
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT;
+const client = new pg.Client(process.env.DATABASE_URL);
+
+client.connect();
+client.on('error', err => console.error(err));
+
 app.use(cors());
 
-
-app.get('/location', (request, response) => {
-  searchToLatLong(request.query.data)
-    .then(location => response.send(location))
-    .catch(error => handleError(error, response));
-})
-
+app.get('/location', getLocation);
 app.get('/weather', getWeather);
 app.get('/yelp', getYelp);
 app.get('/movies', getMovie);
 
 app.listen(PORT, () => console.log(`Listsening on ${PORT}`));
 
-//Helper Functions
-function searchToLatLong(query) {
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GOOGLE_API_KEY}`;
+function getLocation (request, response){
+  Location.lookupLocation({
+    tableName: Location.tableName,
+    query: request.query.data,
 
-  return superagent.get(url)
-    .then(result => {
-      return {
-        search_query: query,
-        formatted_query: result.body.results[0].formatted_address,
-        latitude: result.body.results[0].geometry.location.lat,
-        longitude: result.body.results[0].geometry.location.lng
-      }
-    })
-    .catch(error => handleError(error));
+    cacheHit: function (result) {
+      response.send(result);
+    },
+
+    cacheMiss: function () {
+      const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GOOGLE_API_KEY}`;
+      return superagent.get(url)
+        .then (result => {
+          const location = new Location(this.query, result); 
+          location.save()
+            .then(location => response.send(location));
+        })
+        .catch(error => handleError(error));
+    }
+  })
 }
 
 function getWeather (request, response) {
@@ -83,7 +84,48 @@ function handleError (error, response) {
   if(response) return response.status(500).send('Sorry something went terribly wrong.');
 }
 
+function deleteByLocationId (table, city) {
+  const SQL = `DELETE from ${table} WHERE location_id=${city};`;
+  return client.query(SQL);
+}
+
 //Constructors
+function Location (query, result) {
+  this.search_query = query;
+  this.formatted_query = result.body.results[0].formatted_address;
+  this.latitude = result.body.results[0].geometry.location.lat;
+  this.longitude = result.body.results[0].geometry.location.lng;
+  this.created_at = Date.now()
+}
+
+Location.lookupLocation = (location) => {
+  const SQL = `SELECT * FROM locations WHERE search_query=$1;`;
+  const values = [location.query]; 
+
+  return client.query(SQL, values)
+    .then(result => {
+      if (result.rowCount > 0){
+        location.cacheHit(result.rows[0]);
+      } else {
+        location.cacheMiss();
+      }
+    })
+    .catch(console.error);
+}
+
+Location.prototype = {
+  save: function () {
+    const SQL = `INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING RETURNING id;`;
+    const values = [this.search_query, this.formatted_query, this.latitude, this.longitude];
+
+    return client.query(SQL, values)
+      .then( result => {
+        this.id = result.rows[0].id;
+        return this;
+      });
+  }
+};
+
 function Weather (day) {
   this.time = new Date(day.time * 1000).toString().slice(0, 15);
   this.forecast = day.summary;
